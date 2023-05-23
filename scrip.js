0ae5369c5d367d6b5f6478b91fa08947b135f2bc
Caba <tmcaballeroerfurt@escuelasproa.edu.ar>
2023-05-23 10:59:22 -0300

    hola

---
+++ scrip.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const port = 3000;
+const uri = 'mongodb+srv://jos3:may@cluster0.0xkotzd.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(uri)
+.then((result) => {
+console.log("conectado")
+})
+.catch((error) => {
+console.log("tu error es :"+ error);
+});
+
+
